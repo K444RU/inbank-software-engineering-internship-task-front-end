19038aad4d30fd95e3c7f4e047743d66715be2d8
olegtrofimov <olegtrofimov96@gmail.com>
2024-04-19 13:11:01 +0300

    INB | main js changes -made a slight main.js changes +
    -improved the newLoanApplication, because it didn't send a request in network console
    -improved NegativeDecisionAlert.vue
    -improved PositiveDecisionAlert.vue
    -corrected the mistake of implementing Alert messages in html (:message="")

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app')
+app.use(VueAxios, axios)
+
+app.use(router)
+
+app.mount('#app')
