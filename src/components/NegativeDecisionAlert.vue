7a97d36e99ae5c5752b7358c2e606d1c3a5079eb
olegtrofimov <olegtrofimov96@gmail.com>
2024-04-19 10:38:57 +0300

    INB | Loan decision function -created applyForNewLoan function that would check if •all the required fields are filled in •loan amount is in the allowed amount 2000-10000 •the loan period in allowed months range 12-60 •sends the required params for the request •takes in the response

---
+++ src/components/NegativeDecisionAlert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="negative-decision-alert">
+    <p class="small terms-text">
+      Unfortunately, your loan application was not approved.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NegativeDecisionAlert",
+  props: {
+    message: {}
+  }
+}
+</script>
+
